1e5d8c2e35160c3b33c76e0bdf7a0f4cea9503e3
Leandro Alencar <zzleandrobritozz@gmail.com>
2022-06-18 22:28:21 -0300

    fix: storybook preview

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,13 @@
+import { RouterContext } from 'next/dist/shared/lib/router-context'
 import { ThemeProvider } from 'styled-components'
 import GlobalStyles from 'styles/global'
 import theme from 'styles/theme'
 
+export const parameters = {
+  nextRouter: {
+    Provider: RouterContext.Provider
+  }
+}
 export const decorators = [
   (Story) => (
     <ThemeProvider theme={theme}>
